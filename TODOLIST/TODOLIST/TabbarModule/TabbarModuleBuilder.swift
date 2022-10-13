6299c4072030a3211772b9685a6932374e42b0f0
Burak Dinç <burak.dinc@tsoft.com.tr>
2022-10-13 18:12:51 +0300

    .

--- TODOLIST/TODOLIST/TabbarModule/TabbarModuleBuilder.swift
+++ TODOLIST/TODOLIST/TabbarModule/TabbarModuleBuilder.swift
@@ -11,29 +11,7 @@ class TabbarModuleBuilder: TabbarModuleBuilderProtocol {
     
     static func build() -> UITabBarController {
         let tabbarViewController = TabbarRouter.start()
-        tabbarViewController.tabBar.tintColor = .white
-        tabbarViewController.tabBar.unselectedItemTintColor = .white
-        tabbarViewController.tabBar.isTranslucent = true
-        
-        tabbarViewController.tabBar.layer.masksToBounds = true
-        tabbarViewController.tabBar.layer.cornerRadius = 40
-        tabbarViewController.tabBar.barStyle = .default
-        tabbarViewController.tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        tabbarViewController.tabBar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50)
-        
-        tabbarViewController.tabBar.standardAppearance = TabbarModuleBuilder.setTabbarAppearance()
-        if #available(iOS 15, *) {
-            tabbarViewController.tabBar.scrollEdgeAppearance = TabbarModuleBuilder.setTabbarAppearance()
-        }
-        
         return tabbarViewController
-    }
-    
-    static func setTabbarAppearance() -> UITabBarAppearance {
-        let tabbarAppearance = UITabBarAppearance()
-        tabbarAppearance.backgroundColor = UIColor(named: "appMainColor")
-        tabbarAppearance.backgroundImage = UIImage(named: "material_background_1")
-        return tabbarAppearance
     }
     
 }
--- TODOLIST/TODOLIST/TabbarModule/TabbarVC.swift
+++ TODOLIST/TODOLIST/TabbarModule/TabbarVC.swift
@@ -23,4 +23,42 @@ class TabbarVC: UITabBarController {
         super.viewDidLoad()
         print("TabbarVC--> Yüklendi")
     }
+    
+    override func viewDidLayoutSubviews() {
+        self.setDesign()
+    }
+    
+    private func setDesign() {
+        self.tabBar.isTranslucent = true
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.layer.cornerRadius = 40
+        self.tabBar.barStyle = .default
+        self.tabBar.frame.origin.y = UIScreen.main.bounds.height - 125
+        let width = UIScreen.main.bounds.width - 100
+        self.tabBar.frame.size.width = width
+        self.tabBar.frame.size.height = 80
+        self.tabBar.frame.origin.x = (UIScreen.main.bounds.width / 2) - (width / 2)
+        
+        self.tabBar.standardAppearance = self.setTabbarAppearance()
+        if #available(iOS 15, *) {
+            self.tabBar.scrollEdgeAppearance = self.setTabbarAppearance()
+        }
+        
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = self.tabBar.bounds
+        blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.tabBar.insertSubview(blurView, at: 0)
+    }
+    
+    private func setTabbarAppearance() -> UITabBarAppearance {
+        let tabbarAppearance = UITabBarAppearance()
+        tabbarAppearance.backgroundColor = UIColor(named: "appMainColor")
+        tabbarAppearance.backgroundImage = UIImage(named: "material_background_1")
+        tabbarAppearance.stackedLayoutAppearance.normal.iconColor = .lightGray
+        tabbarAppearance.stackedLayoutAppearance.selected.iconColor = .white
+        tabbarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.lightGray]
+        tabbarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
+        return tabbarAppearance
+    }
 }
